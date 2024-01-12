c861bf95bbf9ab7ecf8ecb6e1fded44aedc05fb8
Harry0815 <hschmidt@edv-schmidt.com>
2024-01-12 15:43:12 +0000

    Readme geändert, Hubs angepasst

--- Hubs/Hubs.cs
+++ Hubs/Hubs.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Runtime.ExceptionServices;
+using System.Text.Json;
+
 
 namespace SignalRChat.Hubs
 {
+    public class msg {
+        public string Last { get; set; }
+        public string First { get; set; }
+
+        public bool Important { get; set; }
+    }
+
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            string t = "{\"user\" : \"Harry\", \"text\": \"Hallo\"}";
+
+            var m = new msg {
+                Last = "Schmidt",
+                First = "Harald",
+                Important = true
+            };
+
+            Console.Write(JsonSerializer.Serialize(m));
+            await Clients.All.SendAsync("ReceiveMessage", user, JsonSerializer.Serialize(m));
         }
     }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Test und Konfiguration C# mit Containern
+SignalRChat
+
+Testen der Signal R Funktionalität
+
+Erstellen der .NET Applikkation
+dotnet new webapp -o SignalRChat
+
+Aufruf mit VS Code
+code -r SignalRChat
+
+oder aber über Solution Explorer im VS Code direkt
+
+
+LibMan ersetzen und neu installieren
+dotnet tool uninstall -g Microsoft.Web.LibraryManager.Cli
+dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+
+SignalR Client JS Scripte installieren
+libman install @microsoft/signalr@latest -p unpkg -d wwwroot/js/signalr --files dist/browser/signalr.js --files dist/browser/signalr.js
+
+
+
